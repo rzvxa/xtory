c41009ccc206a6fa46d04f9c73332b845d24c3cf
rzvxa <rzvxa@protonmail.com>
2022-04-26 06:40:06 +0430

    height fixing

--- src/Project/new.js
+++ src/Project/new.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Form from 'rsuite/Form';
+import ButtonToolbar from 'rsuite/ButtonToolbar';
+import Button from 'rsuite/Button';
+import Input from 'rsuite/Input';
 import Header from 'rsuite/Header';
 import Content from 'rsuite/Content';
 
+
+const Textarea = React.forwardRef((props, ref) => <Input {...props} as="textarea" ref={ref} />);
 export default class New extends React.Component {
   render() {
     return (
@@ -11,10 +16,33 @@ export default class New extends React.Component {
           <h2>New Project</h2>
         </Header>
         <Content>
-          <Link to="/Profile">Go back to home</Link>
-          <div>
-            <img src="https://cdn.britannica.com/13/59613-050-D57A3D88/Vladimir-Ilich-Lenin-1918.jpg?w=400&h=300&c=crop"></img> 
-          </div>
+          <Form fluid>
+            <Form.Group controlId="project-name">
+              <Form.ControlLabel>Project Name</Form.ControlLabel>
+              <Form.Control name="name" />
+            </Form.Group>
+            <Form.Group controlId="project-path">
+              <Form.ControlLabel>Directory</Form.ControlLabel>
+              <Form.Control name="email" type="email" />
+            </Form.Group>
+            <Form.Group controlId="project-description">
+              <Form.ControlLabel>Description</Form.ControlLabel>
+              <Form.Control rows={5} name="textarea" accepter={Textarea} />
+            </Form.Group>
+            <Form.Group controlId="project-description">
+              <Form.ControlLabel>Description</Form.ControlLabel>
+              <Form.Control rows={5} name="textarea" accepter={Textarea} />
+            </Form.Group>
+            <Form.Group controlId="project-description">
+              <Form.ControlLabel>Description</Form.ControlLabel>
+              <Form.Control rows={5} name="textarea" accepter={Textarea} />
+            </Form.Group>
+            <Form.Group>
+              <ButtonToolbar>
+                <Button appearance="primary">Create</Button>
+              </ButtonToolbar>
+            </Form.Group>
+          </Form>
         </Content>
       </div>
     );
