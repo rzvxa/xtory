c7ea2cfe72f24c23a7c7489321b1c988ea1bc7b0
rzvxa <rzvxa@protonmail.com>
2022-05-28 02:51:29 +0430

    Fixed problem with styling

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="page-view">
         <div>Welcome to Xtory 0.1.0</div>
         <div>I would not advise using xtory until version 1.0.0 releases.</div>
       </div>
