5c4ac744b960ea8c620d72f58aabbc35f60e1055
rzvxa <rzvxa@protonmail.com>
2023-05-07 01:00:56 +0330

    Changed eslint no-shadow to tslint-no-shadow

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "warn"
   },
   parserOptions: {
     ecmaVersion: 2020,
