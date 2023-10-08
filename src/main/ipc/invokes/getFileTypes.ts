cde6e4e9ea28fa45b79fb337748d90331e13b585
rzvxa <rzvxa@protonmail.com>
2023-10-08 07:11:22 +0330

    WIP commit, now we are loading flowView node configs from plugins

--- src/main/ipc/invokes/getFileTypes.ts
+++ src/main/ipc/invokes/getFileTypes.ts
@@ -1,9 +1,6 @@
 import { IpcMainInvokeEvent } from 'electron';
-import { PluginConfig } from 'shared/types/plugin';
+import { FileTypeMap } from 'shared/types';
 import project from 'main/project';
-
-// TODO move it to shared types with a better name, Replace similar types with shared one
-type FileTypeMap = { [name: string]: PluginConfig };
 
 export default async function getFileTypes(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
---
+++ src/shared/types/fileTypeMap.ts
@@ -0,0 +1,3 @@
+import { PluginConfig } from './plugin';
+
+export type FileTypeMap = { [name: string]: PluginConfig };
